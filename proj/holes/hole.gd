6c8d3a202baf43570aac4683889f63e00fd0a4d5
Ricky Willis <willis.rf@gmail.com>
2021-09-05 17:15:36 +0100

    more googly eyes

---
+++ proj/holes/hole.gd
@@ -0,0 +1,20 @@
+extends Area2D
+
+export (NodePath) onready var left_eye = get_node(left_eye) as Eye
+export (NodePath) onready var right_eye = get_node(right_eye) as Eye
+
+func _process(_delta):
+	var balls = get_tree().get_nodes_in_group("player")
+	var closestPos = Vector2.INF
+	var closestDist = INF
+	
+	for ball in balls:
+		if ball != self:
+			var dist = global_position.distance_to(ball.global_position)
+			if dist < closestDist:
+				closestDist = dist
+				closestPos = ball.global_position
+	
+	if closestPos != Vector2.INF:
+		left_eye.set_look_at(closestPos)
+		right_eye.set_look_at(closestPos)
