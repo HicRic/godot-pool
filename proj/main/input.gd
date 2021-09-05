f93516c28c8fa38f4b0b5d86da2535fa19b6ad0a
Ricky Willis <willis.rf@gmail.com>
2021-09-05 11:36:54 +0100

    even more input improvements

--- proj/main/input.gd
+++ proj/main/input.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 enum InputState { READY, DRAGGING, COOLDOWN }
 var state = InputState.READY
+var dragging_ball
 
 export (NodePath) onready var arrow = get_node(arrow)
 export (float) var min_drag_len
@@ -15,7 +16,7 @@ func _ready():
 func _on_player_clicked(ball):
 	if state == InputState.READY:
 		change_state(InputState.DRAGGING)
-		arrow.position = ball.position
+		dragging_ball = ball
 		
 func _input(event):
 	if event is InputEventMouseButton and state == InputState.DRAGGING:
@@ -30,8 +31,14 @@ func _input(event):
 				get_tree().call_group("player", "impulse", pos_delta.normalized() * power)
 
 func _process(_delta):
+	if dragging_ball != null:
+		arrow.position = dragging_ball.position
+		
 	var pos_delta = arrow.position - get_global_mouse_position()
-	arrow.rotation = Vector2.UP.angle_to(pos_delta)
+	if state == InputState.DRAGGING:
+		arrow.rotation = Vector2.UP.angle_to(pos_delta)
+		arrow.scale.y = pos_delta.length() / 128.0
+		
 	set_drag_arrow_visible(state == InputState.DRAGGING and pos_delta.length() > min_drag_len)
 
 func change_state(newState):
