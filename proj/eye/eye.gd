be6c56aed005bfd15939f2e306a060571bb2968e
Ricky Willis <willis.rf@gmail.com>
2021-09-05 16:31:29 +0100

    googly eyes

--- proj/eye/eye.gd
+++ proj/eye/eye.gd
@@ -1,15 +1,8 @@
 extends Sprite
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+class_name Eye
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func set_look_at(target):
+	var delta = target - global_position
+	var rot = Vector2.UP.angle_to(delta)
+	($pupil as Node2D).global_rotation = rot
